9c04e44e601351ee6089f2067153fe3ec6c209bc
by-student-2017 <studentsctest@gmail.com>
2022-07-05 13:23:23 +0900

    ubumtu 20.04 lts

--- Si_LAMMPS/ReaxFF/conv_force.sh
+++ Si_LAMMPS/ReaxFF/conv_force.sh
@@ -3,4 +3,4 @@
 fname=$1
 
 nla=`awk '{if($2=="ATOMS"){print NR}}' ${fname}`
-awk -v nla=${nla} '{if(NR>nla){printf " %6d %20.15f %20.15f %20.15f %20.15f %20.15f %20.15f \n",$1,$2,$3,$4,$5,$6,$7}else{print $0}}' ${fname} > ${fname}.reax
+awk -v nla=${nla} '{if(NR>nla){printf " %6d %20.15f %20.15f %20.15f %20.15f %20.15f %20.15f \n",$1,$2,$3,$4,($5*0.043361254529175),($6*0.043361254529175),($7*0.043361254529175)}else{print $0}}' ${fname} > ${fname}.reax
