01409a88cf3b891548b1a20cd88f47c5e2da777e
by-student-2017 <studentsctest@gmail.com>
2022-11-07 15:52:28 +0900

    ubumtu 18.04 lts

--- script/mkinp_FCC.sh
+++ script/mkinp_FCC.sh
@@ -273,3 +273,52 @@ ${ALAMODE_ROOT}/anphon/anphon phband.in > phband.log
 ${ALAMODE_ROOT}/anphon/anphon RTA.in > RTA.log
 EOF
 chmod +x run.sh
+
+cat << EOF > run_harm_step1.sh
+#!/bin/bash
+
+# Binaries
+WIEN2k_init="init_lapw -prec 2n"
+WIEN2k_run="run_lapw -fc 0.1"
+ALAMODE_ROOT=${HOME}/alamode-v.1.4.1/_build
+SC222_data=${name}222.lammps
+
+chmod +x conv_struct.sh
+chmod +x conv_force.sh
+
+# Generate displacement patterns
+${ALAMODE_ROOT}/alm/alm si_alm0.in > alm.log
+
+# Generate structure files
+mkdir displace; cd displace/
+
+python3 ${ALAMODE_ROOT}/tools/displace.py --LAMMPS ../${SC222_data} --prefix harm --mag 0.01 -pf ../si222.pattern_HARMONIC >> run.log
+
+cp ../conv_struct.sh ./
+cp ../conv_force.sh ./
+
+export CURRENT_DIR=\`pwd\`
+EOF
+chmod +x run_harm_step1.sh
+
+cat << EOF > run_harm_step2.sh
+#!/bin/bash
+
+# Binaries 
+ALAMODE_ROOT=${HOME}/alamode-v.1.4.1/_build
+SC222_data=${name}222.lammps
+
+cd ./displace
+
+# Collect data
+python3 ${ALAMODE_ROOT}/tools/extract.py --LAMMPS ../${SC222_data} XFSET.harm* > DFSET_harmonic
+
+cd ../
+
+# Extract harmonic force constants
+${ALAMODE_ROOT}/alm/alm si_alm1.in >> alm.log
+
+# Phonon dispersion
+${ALAMODE_ROOT}/anphon/anphon phband.in > phband.log
+EOF
+chmod +x run_harm_step2.sh
