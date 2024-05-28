caf85353ec0f4646f1d90d1e623cfd1a793dde0e
by-student-2017 <studentsctest@gmail.com>
2024-05-28 11:55:34 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- LAMMPS/C1_Fe2VTaAl_FCC/ML-M3GNet-semi-auto_v1/run_all.sh
+++ LAMMPS/C1_Fe2VTaAl_FCC/ML-M3GNet-semi-auto_v1/run_all.sh
@@ -20,18 +20,40 @@ ALAMODE_ROOT=/mnt/d/alamode-v.1.4.1/_build
 #-----------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------
+# Lammps SNAP potential case
 #coeff=Si_Zuo_Arxiv2019.snapcoeff
 #param=Si_Zuo_Arxiv2019.snapparam
-input_file=in.lmp
-SC222_data=SC222.lammps
 #-----------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------
+input_file=in.lmp
+if [ -f SC444.lammps ]; then
+  SC222_data=SC444.lammps
+else
+  SC222_data=SC222.lammps
+fi
+#-----------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------
+# YxYxY supercell (Y = 2 or 4) to primitive cell for band dispersion.
+sc_type=`echo ${SC222_data:2:1}`
+echo "set YxYxY supercell (Y = ${sc_type}) to primitive cell for band dispersion"
+#-----------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------
+# I feel like there is no problem with leaving it at 12.0.
 distance=12.0
 #-----------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------
+# Anharmonic calculation mode (cubic or random)
+mode=cubic
+echo "set anharmonic calculation mode (cubic or random): ${mode}"
+#-----------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------
 echo "----- Get informaions form ${SC222_data} file -----"
+#-----------------------------------------------------------------------------------------------
 natom=`awk '{if($2=="atoms"){printf "%d",$1}}' ${SC222_data}`
 nat=`awk '{if($2=="atom" && $3=="types"){printf "%d",$1}}' ${SC222_data}`
 nma=`awk '{if($1=="Masses"){print NR}}' ${SC222_data}`
@@ -43,6 +65,7 @@ done
 echo "Number of atom type:" ${nat}
 echo "Elements: "${elem[@]}
 echo "Masses  : "${mass[@]}
+#-----------------------------------------------------------------------------------------------
 la_bohr=`awk '{if($3=="xlo"){printf "%12.6f",($2*1.88973)}}' ${SC222_data}`
 la=`awk '{if($3=="xlo"){printf "%f",$2}}' ${SC222_data}`
 xx=`awk -v la=${la} '{if($3=="xlo"){printf "%12.6f",($2/la)}}' ${SC222_data}`
@@ -51,6 +74,7 @@ zz=`awk -v la=${la} '{if($3=="zlo"){printf "%12.6f",($2/la)}}' ${SC222_data}`
 xy=`awk -v la=${la} 'BEGIN{XY=0.0}{if($4=="xy"){XY=$1}}END{printf "%12.6f",(XY/la)}' ${SC222_data}`
 xz=`awk -v la=${la} 'BEGIN{XZ=0.0}{if($5=="xz"){XZ=$2}}END{printf "%12.6f",(XZ/la)}' ${SC222_data}`
 yz=`awk -v la=${la} 'BEGIN{YZ=0.0}{if($6=="yz"){YZ=$3}}END{printf "%12.6f",(YZ/la)}' ${SC222_data}`
+#-----------------------------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------------------------
 ## VASP: [A] + [fx,fy,fz], Lammps (Ovito): [A] + [x,y,z], Alamode (alm): [A] + [fx,fy,fz]
@@ -59,6 +83,8 @@ yz=`awk -v la=${la} 'BEGIN{YZ=0.0}{if($6=="yz"){YZ=$3}}END{printf "%12.6f",(YZ/l
 ## Calculating the inverse of a 3x3 lower triangular matrix
 #-----------------------------------------------------------------------------------------------
 det=`echo "${xx} ${yy} ${zz}" | awk '{printf "%f",($1*$2*$3)}'`
+vol=`echo "${det} ${la}" | awk '{printf "%f",($1*$2^3)}'`
+random_num=`echo "${vol}" | awk '{printf "%d",int(20*$1/(5.68^3)+0.5)}'`
 #-----------------------------------------------------------------------------------------------
 #ixx=`echo "${xx} ${yy} ${zz} ${xy} ${xz} ${yz} ${det}" | awk '{printf "%f", ($2*$3-0.0)/$7}'`
 #iyy=`echo "${xx} ${yy} ${zz} ${xy} ${xz} ${yz} ${det}" | awk '{printf "%f", ($1*$3-0.0)/$7}'`
@@ -73,22 +99,32 @@ ixz=`echo "${xx} ${yy} ${zz} ${xy} ${xz} ${yz} ${det} " | awk '{printf "%f", ($4
 iyz=`echo "${xx} ${yy} ${zz} ${xy} ${xz} ${yz} ${det} " | awk '{printf "%f",-($1*$6-$4*$5)/$7}'`
 #-----------------------------------------------------------------------------------------------
 
+#-----------------------------------------------------------------------------------------------
+# lattice vector of lammps
 a1=`echo "${xx}   0.0   0.0 ${la} " | awk '{printf "%f",($4*($1^2+$2^2+$3^2)^0.5)}'`
 a2=`echo "${xy} ${yy}   0.0 ${la} " | awk '{printf "%f",($4*($1^2+$2^2+$3^2)^0.5)}'`
 a3=`echo "${xz} ${yz} ${zz} ${la} " | awk '{printf "%f",($4*($1^2+$2^2+$3^2)^0.5)}'`
+#-----------------------------------------------------------------------------------------------
 
+#-----------------------------------------------------------------------------------------------
 #Note: 1/0.529176 = 1.88973
-la_bohr_d2=`awk '{if($3=="xlo"){printf "%-12.6f",($2/2/0.529)}}' ${SC222_data}`
-la_bohr_d4=`awk '{if($3=="xlo"){printf "%-12.6f",($2/4/0.529)}}' ${SC222_data}`
-la_bohr_r3=`awk '{if($3=="xlo"){printf "%-12.6f",($2/2/0.529*0.8660)}}' ${SC222_data}`
+la_bohr_primitive=`awk -v sc_type=${sc_type} '{if($3=="xlo"){printf "%-12.6f",($2/sc_type/0.529)}}' ${SC222_data}` # 2x2x2 supercell vs. 1x1x1 primitive cell
+la_bohr_r3=`awk -v sc_type=${sc_type} '{if($3=="xlo"){printf "%-12.6f",($2/sc_type/0.529*0.8660)}}' ${SC222_data}` 
+#-----------------------------------------------------------------------------------------------
 
 #--------------------------------
+# Set the elements to be calculated in the lammps control file template (in_tmp.lmp) and output it as in.lmp.
 cp in_tmp.lmp in.lmp
 els=${elem[@]}
 sed -i "s/XXXXXX/${els}/g" in.lmp
 #--------------------------------
 
 #-----------------------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+#### alm0.log
+if [ ! -e alm0.log ]; then
 
 echo "----- Generate displacement patterns -----"
 cat << EOF > alm0.in
@@ -125,25 +161,57 @@ echo "/" >> alm0.in
 
 ${ALAMODE_ROOT}/alm/alm alm0.in > alm0.log
 
+fi
+#### alm0.log
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# Get information on the number of files for calculating the structure with displacement using lammps.
 grep "Space group" alm0.log
 grep "Number of disp. patterns" alm0.log
 NHARM=`awk '{if($1=="Number" && $3=="disp." && $6=="HARMONIC"){printf "%d",$8}}' alm0.log`
 echo "harmonic file: ${NHARM}"
 NANHA=`awk '{if($1=="Number" && $3=="disp." && $6=="ANHARM3"){printf "%d",$8}}' alm0.log`
 echo "anharmonic file: ${NANHA}"
+#-------------------------------------------------------------------------------
+
 
 echo "----- Generate structure files of LAMMPS (displace.py) -----"
 mkdir displace; cd displace
 
-python3 ${ALAMODE_ROOT}/tools/displace.py --LAMMPS ../${SC222_data} --prefix harm --mag 0.01 -pf ../sc222.pattern_HARMONIC >> run.log
-python3 ${ALAMODE_ROOT}/tools/displace.py --LAMMPS ../${SC222_data} --prefix cubic --mag 0.04 -pf ../sc222.pattern_ANHARM3 >> run.log
+
+#-------------------------------------------------------------------------------
+#### run.log
+if [ ! -e run.log ]; then
+  python3 ${ALAMODE_ROOT}/tools/displace.py --LAMMPS ../${SC222_data} --prefix harm --mag 0.01 -pf ../sc222.pattern_HARMONIC >> run.log
+  
+  if [ ${mode} == "cubic" ]; then
+    echo "cubic displacement case"
+    python3 ${ALAMODE_ROOT}/tools/displace.py --LAMMPS ../${SC222_data} --prefix ${mode} --mag 0.04 -pf ../sc222.pattern_ANHARM3 >> run.log
+  else
+    echo "random displacement case"
+    python3 ${ALAMODE_ROOT}/tools/displace.py --LAMMPS ../${SC222_data} --prefix ${mode} --random --mag 0.04 -nd ${random_num} >> run.log
+  fi
+fi
+#### run.log
+#-------------------------------------------------------------------------------
+
 
 #cp ./../${coeff} ./
 #cp ./../${param} ./
 cp ./../${input_file} ./
 
+
+#-------------------------------------------------------------------------------
 echo "----- Run LAMMPS -----"
-NHARM_restart=`cat NHARM_restart.txt`
+#-------------------------------------------------------------------------------
+if [ -f NHARM_restart.txt ]; then
+  NHARM_restart=`cat NHARM_restart.txt`
+else
+  NHARM_restart=0
+fi
+if [ ! ${NHARM_restart} -eq ${NHARM} ]; then
 for i in $(seq -w ${NHARM_restart} ${NHARM})
 do
    cp harm${i}.lammps tmp.lammps
@@ -152,26 +220,54 @@ do
    echo "----- XFSET.harm${i} / ${NHARM} -----"
    echo ${i} > NHARM_restart.txt
 done
-
-NANHA_restart=`cat NANHA_restart.txt`
+else
+  echo "step ${NHARM_restart} / ${NHARM} case"
+  echo "----- skip HARMONIC calculation -----"
+fi
+#-------------------------------------------------------------------------------
+if [ -f NANHA_restart.txt ]; then
+  NANHA_restart=`cat NANHA_restart.txt`
+else
+  NANHA_restart=0
+fi
+if [ ! ${NANHA_restart} -eq ${NANHA} ]; then
 for i in $(seq -w ${NANHA_restart} ${NANHA})
 do
-   cp cubic${i}.lammps tmp.lammps
+   cp ${mode}${i}.lammps tmp.lammps
    $LAMMPS < ${input_file} >> run.log
-   mv XFSET XFSET.cubic${i}
-   echo "----- XFSET.cubic${i} / ${NANHA} -----"
+   mv XFSET XFSET.${mode}${i}
+   echo "----- XFSET.${mode}${i} / ${NANHA} -----"
    echo ${i} > NANHA_restart.txt
 done
+else
+  echo "step ${NANHA_restart} / ${NANHA} case"
+  echo "----- skip ANHARM3 calculation -----"
+fi
+#-------------------------------------------------------------------------------
 
 
+#-------------------------------------------------------------------------------
 echo "----- Collect data (extract.py) -----"
-python3 ${ALAMODE_ROOT}/tools/extract.py --LAMMPS ../${SC222_data} XFSET.harm* > DFSET_harmonic
-python3 ${ALAMODE_ROOT}/tools/extract.py --LAMMPS ../${SC222_data} XFSET.cubic* > DFSET_cubic
+#-------------------------------------------------------------------------------
+if [ ! -e DFSET_harmonic ]; then
+  python3 ${ALAMODE_ROOT}/tools/extract.py --LAMMPS ../${SC222_data} XFSET.harm* > DFSET_harmonic
+fi
+#-------------------------------------------------------------------------------
+if [ ! -e DFSET_${mode} ]; then
+  python3 ${ALAMODE_ROOT}/tools/extract.py --LAMMPS ../${SC222_data} XFSET.${mode}* > DFSET_${mode}
+fi
+#-------------------------------------------------------------------------------
 
-cd ../
+
+cd ./../
 
 
+#-------------------------------------------------------------------------------
 echo "----- Extract harmonic force constants (alm1.in) -----"
+#-------------------------------------------------------------------------------
+#### alm1.log
+if [ ! -e alm1.log ]; then
+
 sed -e "s/PREFIX = sc222/PREFIX = sc222_harm/" alm0.in > alm1.in
 sed -i "s/suggest/optimize/" alm1.in
 sed -i "/&interaction/i &optimize" alm1.in
@@ -180,19 +276,55 @@ sed -i "/&interaction/i /" alm1.in
 sed -i "/&interaction/i \ " alm1.in
 sed -i "s/NORDER = 2/NORDER = 1/" alm1.in
 ${ALAMODE_ROOT}/alm/alm alm1.in > alm1.log
+
+fi
+#### alm1.log
+#-------------------------------------------------------------------------------
 grep "Space group" alm1.log
-grep "Fitting error" alm1.log
+grep "Fitting error" alm1.logs
+#-------------------------------------------------------------------------------
 
 
-echo "----- Extract cubic force constants (alm2.in) -----"
-sed -e "s/PREFIX = sc222_harm/PREFIX = sc222_cubic/" alm1.in > alm2.in
-sed -i "s/DFSET_harmonic/DFSET_cubic/" alm2.in
-sed -i "/DFSET = displace\/DFSET_harmonic/a \  FC2XML = sc222_harm.xml" alm2.in
+#-------------------------------------------------------------------------------
+echo "----- Extract ${mode} force constants (alm2.in) -----"
+#-------------------------------------------------------------------------------
+#### alm2.log
+if [ ! -e alm2.log ]; then
+
+sed -e "s/PREFIX = sc222_harm/PREFIX = sc222_${mode}/" alm1.in > alm2.in
+sed -i "s/DFSET_harmonic/DFSET_${mode}/" alm2.in
+if [ ${mode} == "cubic" ]; then
+  sed -i "/DFSET = displace\/DFSET_harmonic/a \  FC2XML = sc222_harm.xml" alm2.in
+else
+  sed -i "/DFSET = displace\/DFSET_harmonic/a \  FC2XML = sc222_harm.xml/a \  LMODEL = enet/a \  NDATA = ${random_num}/a \ CV = 4/a \ L1_RATIO = 1.0/a \ CONV_TOL = 1.0e-8" alm2.in
+fi
 sed -i "s/NORDER = 1/NORDER = 2/" alm2.in
 ${ALAMODE_ROOT}/alm/alm alm2.in > alm2.log
+
+#-------------------------------------------------------------------
+if [ ${mode} == "random" ]; then
+# opt.in
+alpha=`awk '{if($1=="Minimum" && $2=="CVSCORE"){print $6}}' alm.log`
+echo "Minimum CVSCORE at alpha = "${alpha}
+sed "15i \ L1_ALPHA = ${alpha}" sc_alm2.in > sc_opt.in
+sed -i "s/CV = 4/CV = 0/" sc_opt.in
+awk '{if($1=="CV"){print $0}}' sc_opt.in
+awk '{if($1=="L1_ALPHA"){print $0}}' sc_opt.in
+${ALAMODE_ROOT}/alm/alm sc_opt.in >> alm.log
+fi
+#-------------------------------------------------------------------
+
+fi
+#### alm2.log
+#-------------------------------------------------------------------------------
 grep "Space group" alm2.log
 grep "Fitting error" alm2.log
+#-------------------------------------------------------------------------------
 
+
+#-------------------------------------------------------------------------------
+#### phband.log
+if [ ! -e phband.log ]; then
 
 echo "----- Phonon dispersion (phband.in) -----"
 cat << EOF > phband.in
@@ -213,7 +345,7 @@ if [ ${sg:0:1} = "F" ]; then
   SG=FCC
 cat << EOF >> phband.in
 &cell
-  ${la_bohr_d4} # factor in Bohr unit
+  ${la_bohr_primitive} # factor in Bohr unit
   0.0 0.5 0.5
   0.5 0.0 0.5
   0.5 0.5 0.0
@@ -230,7 +362,7 @@ elif [ ${sg:0:1} = "I" ]; then
   SG=BCC
 cat << EOF >> phband.in
 &cell
-  ${la_bohr_d4} # factor in Bohr unit
+  ${la_bohr_primitive} # factor in Bohr unit
   1.0 0.0 0.0
   0.0 1.0 0.0
   0.0 0.0 1.0
@@ -248,7 +380,7 @@ elif [ ${sg:0:8} = "P6_3/mmc" ]; then
   SG=HCP
 cat << EOF >> phband.in
 &cell
-  ${la_bohr_d4} # factor in Bohr unit
+  ${la_bohr_primitive} # factor in Bohr unit
   1.00000 0.00000 0.00000
  -0.50000 0.86603 0.00000
   0.00000 0.00000 ${zz}
@@ -266,7 +398,7 @@ else
   SG=SC
 cat << EOF >> phband.in
 &cell
-  ${la_bohr_d4}
+  ${la_bohr_primitive}
   ${xx} 0.0   0.0   # a1
   ${xy} ${yy} 0.0   # a2
   ${xz} ${yz} ${zz} # a3
@@ -309,13 +441,21 @@ fi
 
 ${ALAMODE_ROOT}/anphon/anphon phband.in > phband.log
 
+fi
+#### phband.log
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+#### RTA.log
+if [ ! -e RTA.log ]; then
 
 echo "----- Thermal conductivity (RTA.in) -----"
 cat << EOF > RTA.in
 &general
   PREFIX = sc222_10
   MODE = RTA
-  FCSXML = sc222_cubic.xml
+  FCSXML = sc222_${mode}.xml
 
   NKD = ${nat}; KD = ${elem[@]}
   MASS = ${mass[@]}
@@ -325,7 +465,7 @@ EOF
 if [ ${sg:0:1} = "F" ]; then
 cat << EOF >> RTA.in
 &cell
-  ${la_bohr_d4}
+  ${la_bohr_primitive}
   0.0 0.5 0.5
   0.5 0.0 0.5
   0.5 0.5 0.0
@@ -334,7 +474,7 @@ EOF
 elif [ ${sg:0:1} = "I" ]; then
 cat << EOF >> RTA.in
 &cell
-  ${la_bohr_d4} # factor in Bohr unit
+  ${la_bohr_primitive} # factor in Bohr unit
   1.0 0.0 0.0
   0.0 1.0 0.0
   0.0 0.0 1.0
@@ -344,7 +484,7 @@ elif [ ${sg:0:8} = "P6_3/mmc" ]; then
   echo "space group: "${sg:0:8}" (HCP) settings"
 cat << EOF >> RTA.in
 &cell
-  ${la_bohr_d4}
+  ${la_bohr_primitive}
   1.00000 0.00000 0.00000
  -0.50000 0.86603 0.00000
   0.00000 0.00000 ${zz}
@@ -354,7 +494,7 @@ else
   echo "space group: "${sg}" (P) settings"
 cat << EOF >> RTA.in
 &cell
-  ${la_bohr_d4}
+  ${la_bohr_primitive}
   ${xx} 0.0   0.0   # a1
   ${xy} ${yy} 0.0   # a2
   ${xz} ${yz} ${zz} # a3
@@ -362,13 +502,17 @@ cat << EOF >> RTA.in
 EOF
 fi
 
+#----------------------------------------------------------------------
+# 2x2x2 supercell vs. 1x1x1 primitive cell
 #nqx=`echo ${xx} ${la} | awk '{printf "%d",int(12*5.68/($1*$2)+0.5)}'`
 #nqy=`echo ${yy} ${la} | awk '{printf "%d",int(12*5.68/($1*$2)+0.5)}'`
 #nqz=`echo ${zz} ${la} | awk '{printf "%d",int(12*5.68/($1*$2)+0.5)}'`
 #----------------------------------------------------------------------
+# 4x4x4 supercell vs. 1x1x1 primitive cell
 nqx=`echo ${xx} ${la} | awk '{printf "%d",int(12*5.68/($1*$2)+0.5)*2}'`
 nqy=`echo ${yy} ${la} | awk '{printf "%d",int(12*5.68/($1*$2)+0.5)*2}'`
 nqz=`echo ${zz} ${la} | awk '{printf "%d",int(12*5.68/($1*$2)+0.5)*2}'`
+#----------------------------------------------------------------------
 
 cat << EOF >> RTA.in
 &kpoint
@@ -379,6 +523,14 @@ EOF
 
 ${ALAMODE_ROOT}/anphon/anphon RTA.in > RTA.log
 
+fi
+#### RTA.log
+#-------------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------------------------
+# I wrote awk as a one-liner to make it easier to check, but it is easier to read with line breaks.
+# I realized later that I had written the legend function in a way that lacked flexibility. (for "band-%-d" sequence)
 #-----------------------------------------------------------------------------------------------
 awk '{if(NR==3){printf "# k-axis, Phonon frequency [THz]";for(j=1;j<=(NF+3);j++){printf ", band-%-d",j};printf("\n")}else if(NR>=4){printf("%6d %12.6f"),(NR-3),$1;for(i=2;i<=NF;i++){printf("%12.6f ",$i*0.029979)}{printf("\n")}}}' sc222.bands > sc222_THz.bands
 awk '{if(NR==3){printf "# k-axis, Phonon energy [meV]";for(j=1;j<=(NF+3);j++){printf ", band-%-d",j};printf("\n")}else if(NR>=4){printf("%6d %12.6f"),(NR-3),$1;for(i=2;i<=NF;i++){printf("%12.6f ",$i*0.12398)}{printf("\n")}}}' sc222.bands > sc222_meV.bands
