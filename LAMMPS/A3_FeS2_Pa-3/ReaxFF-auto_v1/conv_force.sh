e1182fa62f384b51e929b29fc47b02b2296fb4e7
by-student-2017 <studentsctest@gmail.com>
2024-05-29 18:47:36 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- LAMMPS/A3_FeS2_Pa-3/ReaxFF-auto_v1/conv_force.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-fname=$1
-
-nla=`awk '{if($2=="ATOMS"){print NR}}' ${fname}`
-awk -v nla=${nla} '{if(NR>nla){printf " %6d %20.15f %20.15f %20.15f %20.15f %20.15f %20.15f \n",$1,$2,$3,$4,($5*0.043361254529175),($6*0.043361254529175),($7*0.043361254529175)}else{print $0}}' ${fname} > ${fname}.reax
